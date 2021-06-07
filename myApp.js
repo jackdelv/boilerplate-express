c2ae01486301bf0da62782387ccf0f314d8662a3
jackdelv <jackdelv39@gmail.com>
2021-06-07 14:48:43 -0400

    Added a simple log to show requests

--- myApp.js
+++ myApp.js
@@ -2,6 +2,12 @@ var express = require('express');
 var app = express();
 console.log("Hello World");
 //app.get("/", function(req, res) { res.send("Hello Express")})
+app.use(
+	function(req, res, next) {
+		console.log(req.method + " " + req.path + " - " + req.ip);
+		next();
+	}
+)
 app.get("/", function(req, res) {res.sendFile(__dirname + "/views/index.html")})
 
 app.use("/public", express.static(__dirname + "/public"));
@@ -9,14 +15,12 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/json", 
 	function(req, res) {
 		if (process.env.MESSAGE_STYLE == "uppercase") {
-			res.json({"message": "HELLO JSON"})
+			res.json({"message": "HELLO JSON"});
 		} else {
-			res.json({"message": "Hello json"})
+			res.json({"message": "Hello json"});
 		}
 	}
 )
-
-
 
 
 
