545891df78690e0ce1356e8ef26f4cdbb6a86a72
jackdelv <jackdelv39@gmail.com>
2021-06-07 14:14:29 -0400

    Added Middleware to add styling

--- myApp.js
+++ myApp.js
@@ -4,7 +4,7 @@ console.log("Hello World");
 //app.get("/", function(req, res) { res.send("Hello Express")})
 app.get("/", function(req, res) {res.sendFile(__dirname + "/views/index.html")})
 
-
+app.use("/public", express.static(__dirname + "/public"));
 
 
 
