65c007be273d2b1b44d7e307098c01c36d6dd7f2
jackdelv <jackdelv39@gmail.com>
2021-06-07 14:26:35 -0400

    Added environment variable for cases

--- myApp.js
+++ myApp.js
@@ -6,7 +6,15 @@ app.get("/", function(req, res) {res.sendFile(__dirname + "/views/index.html")})
 
 app.use("/public", express.static(__dirname + "/public"));
 
-app.get("/json", function(req, res) {res.json({"message": "Hello json"})})
+app.get("/json", 
+	function(req, res) {
+		if (process.env.MESSAGE_STYLE == "uppercase") {
+			res.json({"message": "HELLO JSON"})
+		} else {
+			res.json({"message": "Hello json"})
+		}
+	}
+)
 
 
 
