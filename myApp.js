0ea7e844c4d62a0023dc231baeefc26fdcfbbc09
jackdelv <jackdelv39@gmail.com>
2021-06-07 13:45:30 -0400

    Added second serve

--- myApp.js
+++ myApp.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
 console.log("Hello World");
-app.get("/", function(req, res) { res.send("Hello Express")})
-
+//app.get("/", function(req, res) { res.send("Hello Express")})
+app.get("/", res.sendFile("/boilerplate-npm/views/index.html"))
 
 
 
